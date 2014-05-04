c6ddcc3326f6159847b8f399d7e44febb012455c
Ladinu Chandrasinghe <ladinu@gmail.com>
2014-05-03 17:35:55 -0700

    a

---
+++ hw2/src/test/java/com/compiler/hw2/testGenProgram.java
@@ -0,0 +1,12 @@
+package com.compiler.hw2;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class testGenProgram {
+
+   @Test
+   public void testGen() throws Exception {
+      Assert.assertEquals(1, 1);
+   }
+}
